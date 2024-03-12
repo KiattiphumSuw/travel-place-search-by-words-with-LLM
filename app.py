66696c27bad59eff480429455d372e96eca5e11f
chaphowasit mahayossanan <bigrockbeam@gmail.com>
2024-03-13 00:44:53 +0700

    weaviate combination

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
+import weaviate
+import weaviate.classes as wvc
+import os
+import requests
+import json
 
 from flask import Flask,request,render_template
+from flask import make_response, jsonify
 from json import loads,dumps,load
 
 app = Flask(__name__)
@@ -26,3 +32,29 @@ def get_user():
         return make_response(jsonify({'message': 'succeed'}), 200)
     except Exception as e:
         return make_response(jsonify({'message': e}), 500)
+    
+@app.route('/v1/nodes/Question2', methods=['GET'])
+def get_result():
+    client = weaviate.connect_to_wcs(
+        cluster_url="xxx",
+        auth_credentials=weaviate.auth.AuthApiKey("xxx"),
+        headers={
+            "xxx"  # Replace with your inference API key
+        }
+    )
+
+    try:
+        pass # Replace with your code. Close client gracefully in the finally block.
+        questions = client.collections.get("Question2")
+
+        response = questions.query.near_text(
+            query="thai food",
+            limit=2
+        )
+
+        result = response.objects[0].properties
+    except Exception as e:
+        return make_response(jsonify({'message': e}), 500)
+    finally:
+        client.close()  
+    return make_response(jsonify({'message': "hello"}), 200)
