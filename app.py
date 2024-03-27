60ceb198548ebcc59cb784894ab0c74d5fead26b
chaphowasit mahayossanan <bigrockbeam@gmail.com>
2024-03-27 19:20:21 +0700

    fixed weaviate connection and html

--- app.py
+++ app.py
@@ -2,13 +2,9 @@
 import weaviate
 import weaviate.classes as wvc
 import os
-import requests
-import json
 
 from flask import Flask,request,render_template
 from flask import make_response, jsonify
-from json import loads,dumps,load
-
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -37,7 +33,7 @@ def get_user():
     except Exception as e:
         return make_response(jsonify({'message': e}), 500)
     
-@app.route('/v1/nodes/Question2', methods=['GET'])
+@app.route('/v1/nodes/travelPlanner', methods=['GET'])
 def get_result():
     try:
         query = request.args.get('query')
@@ -45,43 +41,38 @@ def get_result():
             return make_response(jsonify({'message': 'Query parameter is missing.'}), 400)
 
         client = weaviate.connect_to_wcs(
-            cluster_url=CLUSTER_URL,
-            auth_credentials=weaviate.auth.AuthApiKey(WEAVIATE_AuthApiKey),
-            headers={
-                "X-OpenAI-Api-Key": GPT_KEY1
-            }
-        )
+                cluster_url=CLUSTER_URL,
+                auth_credentials=weaviate.auth.AuthApiKey(WEAVIATE_AuthApiKey),
+                headers={
+                    "X-OpenAI-Api-Key": GPT_KEY1  # Replace with your inference API key
+                }
+            )
 
-        questions = client.collections.get("Review")
+        questions = client.collections.get("Review7")
         result_all = []
 
         response_re = questions.query.near_text(
             query=query,
-            limit=2
+            limit=5
         )
 
-        resname_review = response_re.objects[0].properties["restaurants"]
-        review_review = response_re.objects[0].properties["reviews"]
+        questions = client.collections.get("Description")
+        for result_object in response_re.objects:
+            resname_review = result_object.properties["restaurant_name"]
+            review_review = result_object.properties["reviews"]
 
-        questions = client.collections.get("Question2")
-        response_des = questions.query.fetch_objects(
-        filters=wvc.query.Filter.by_property("restaurant_name").contains_any([resname_review]),
-        limit=1
-    )
-        result_des = []
-        for o in response_des.objects:
-            result_des.append(o.properties)  
-            
-        result_all.extend(result_des)
-        result_all.append({"review":review_review})
+            response_des = questions.query.fetch_objects(
+                filters=wvc.query.Filter.by_property("restaurant_name").contains_any([resname_review]),
+                limit=1
+            )
 
-    #     response_gpt = questions.generate.near_text(
-    #     query="biology",
-    #     limit=2,
-    #     single_prompt="Explain {answer} as you might to a five-year-old."
-    # )
+            result_des = []
+            for o in response_des.objects:
+                result_des.append(o.properties)
 
-    #     response_gpt = response_gpt.objects[0].generated  # Inspect the generated text
+            result_all.extend(result_des)
+            result_all.append({"review": review_review})
+            print(result_all)
             
         return make_response(jsonify({'message': result_all}), 200)
     except weaviate.exceptions.WeaviateGRPCUnavailableError as e:
